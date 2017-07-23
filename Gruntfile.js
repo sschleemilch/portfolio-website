da323abe85fc83eb17c2cb83d50359793ea175da
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-07-23 20:15:57 +0200

    working environment, jshint, uglify, sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,33 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     grunt.initConfig({
         jshint: {
             all: ['src/**/*.js']
         },
         uglify: {
-        	target: {
-        		src: 
+        	files: {
+        		src: 'src/**/*.js',
+        		dest: 'src/jsm/',
+        		ext: '.min.js',
+        		expand: true,
+        		flatten: true
+        	}
+        },
+        sass: {
+        	all: {
+        		files: {
+        			'src/css/style.css' : 'src/sass/style.scss'
+        		}
         	}
         },
         watch: {
-        	files: ['src/**/*.js'],
-        	tasks: ['jshint', 'concat', 'uglify']
+        	files: ['src/**/*.js', 'src/**/*.scss'],
+        	tasks: ['jshint', 'uglify', 'sass']
         }
     });
+
+    grunt.registerTask('default', ['watch']);
 };
