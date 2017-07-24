313d2f7a8b1ccd1a16516b30bf63d5c7689dcc6a
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-07-24 19:27:31 +0200

    adaption

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,17 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+ 	grunt.loadNpmTasks('grunt-newer');
 
     grunt.initConfig({
         jshint: {
-            all: ['src/**/*.js']
+            all: ['src/js/**/*.js']
         },
         uglify: {
-        	files: {
-        		src: 'src/**/*.js',
+        	all: {
+        		src: ['src/js/**/*.js'],
         		dest: 'src/jsm/',
         		ext: '.min.js',
         		expand: true,
@@ -25,10 +26,9 @@ module.exports = function(grunt) {
         	}
         },
         watch: {
-        	files: ['src/**/*.js', 'src/**/*.sass'],
-        	tasks: ['jshint', 'uglify', 'sass']
+        	files: ['src/js/**/*.js', 'src/**/*.sass'],
+        	tasks: ['newer:jshint:all', 'newer:uglify:all', 'newer:sass:all']
         }
     });
-
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['jshint:all', 'uglify:all', 'sass:all', 'watch']);
 };
