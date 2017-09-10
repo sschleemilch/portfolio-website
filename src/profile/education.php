0166b2d9164f1245810dcc88e729eaab5bda451b
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-09-10 22:46:54 +0200

    timelines work, edu

--- src/profile/education.php
+++ src/profile/education.php
@@ -9,58 +9,80 @@
 <section class="hero">
 	<div class="hero-body">
 		<div class="container">
-			<h1 class="title">University education</h1>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">2013-2016:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">Master</h1>
-						<h2 class="subtitle is-5">Automotive Software Engineering</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.tum.de/"><img src="/img/logo_tum.png" alt="Logo:TUM-University" width="150" height="79"></a>
-					</div>
-				</div>
-			</div>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">2010-2013:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">Bachelor</h1>
-						<h2 class="subtitle is-5">Engineer in Mechatronics</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.fau.de/"><img src="/img/logo_fau_blue.png" alt="Logo:FAU-University" width="150" height="64"></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<section class="hero">
-	<div class="hero-body">
-		<div class="container">
-			<h1 class="title">School education</h1>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">2000-2009:</h1>
-					</div>
-					<div class="column is-7">
-						<h1 class="title is-3">Abitur</h1>
-						<h2 class="subtitle is-5">Qualification for university entrance</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="http://www.sigmund-schuckert-gymnasium.de/wordpress/"><img src="/img/logo_ssg.png" alt="Logo:SSG-Gymnasium" width="110" height="80"></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+            <h1 class="title">Education</h1>
+            <ul class="timeline">
+                <li class="timeline-header">
+                    <span class="tag is-large is-info">03/2016</span>
+                </li>
+	           <li class="timeline-item is-info">
+                    <div class="timeline-marker"></div>
+                    <div class="timeline-content is-100">
+            			<div class="lb is-blue is-step">
+            				<div class="columns">
+            					<div class="column is-2">
+            						<h1 class="title is-5">2013-2016:</h1>
+            					</div>
+            					<div class="column is-7 has-text-left">
+            						<h1 class="title is-3">Master</h1>
+            						<h2 class="subtitle is-5">Automotive Software Engineering</h2>
+            					</div>
+            					<div class="column is-3 has-text-right">
+            						<a href="https://www.tum.de/"><img src="/img/logo_tum.png" alt="Logo:TUM-University" width="150" height="79"></a>
+            					</div>
+            				</div>
+            			</div>
+                    </div>
+                </li>
+                 <li class="timeline-header">
+                    <span class="tag is-medium is-dark">2013</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker"></div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">2010-2013:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">Bachelor</h1>
+                                    <h2 class="subtitle is-5">Engineer in Mechatronics</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.fau.de/"><img src="/img/logo_fau_blue.png" alt="Logo:FAU-University" width="150" height="64"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-medium is-dark">2009 - End of basic school</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker"></div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">2000-2009:</h1>
+                                </div>
+                                <div class="column is-7">
+                                    <h1 class="title is-3">Abitur</h1>
+                                    <h2 class="subtitle is-5">Qualification for university entrance</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="http://www.sigmund-schuckert-gymnasium.de/wordpress/"><img src="/img/logo_ssg.png" alt="Logo:SSG-Gymnasium" width="110" height="80"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-large is-info">2000</span>
+                </li>
+            </ul>
+        </div>
+    </div>
 </section>
 
 <?php
--- src/profile/work.php
+++ src/profile/work.php
@@ -11,121 +11,209 @@
 	<div class="hero-body">
 		<div class="container">
 			<h1 class="title">Work experience</h1>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">06/2016-present:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">Bosch GmbH</h1>
-						<h2 class="subtitle is-5">Software Engineer: Software Integration</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.bosch.com/de/"><img src="/img/logo_bosch.png" alt="Logo:Bosch_GmbH" width="200" height="45"></a>
-					</div>
-				</div>
-                <hr> 
-                <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_bosch','work_details_bosch'); this.blur();" id="btn_work_details_bosch">
-                  <span>Details</span>
-                  <span class="icon is-small">
-                    <i class="fa fa-angle-down" aria-hidden="true"></i>
-                  </span>
-                </button>
-                <span><br></span>
-                <div class="columns" id="work_details_bosch" style="display: none;">
-                    <div class="column is-2">
-                        <h1 class="title is-5">10/2014-07/2015:</h1>
+            <ul class="timeline">
+                <li class="timeline-header">
+                    <span class="tag is-large is-info">Present</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
                     </div>
-                    <div class="column is-7 has-text-left">
-                        <h1 class="title is-3">BMW M GmbH</h1>
-                        <h2 class="subtitle is-5">Working Student: OBD department</h2>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step" style="min-width: 200px">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">06/2016-present:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">Bosch GmbH</h1>
+                                    <h2 class="subtitle is-5">Software Engineer: Software Integration</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.bosch.com/de/"><img src="/img/logo_bosch.png" alt="Logo:Bosch_GmbH" width="200" height="45"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_bosch','work_details_bosch'); this.blur();" id="btn_work_details_bosch">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_bosch" style="display: none;">
+                                <h1 class="title is-4">Job Position:</h1>
+                            </div>
+                        </div>
                     </div>
-                    <div class="column is-3 has-text-right">
-                        <a href="https://www.bmw-m.com/de/index.html"><img src="/img/logo_bmw.png" alt="Logo:BMW_M_GmbH" width="100" height="100"></a>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-medium is-dark">03/2016 - End of Studies</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
                     </div>
-                </div>
-			</div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">10/2014-07/2015:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">BMW M GmbH</h1>
+                                    <h2 class="subtitle is-5">Working Student: OBD department</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.bmw-m.com/de/index.html"><img src="/img/logo_bmw.png" alt="Logo:BMW_M_GmbH" width="100" height="100"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_bmw','work_details_bmw'); this.blur();" id="btn_work_details_bmw">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_bmw" style="display: none;">
+                                <p> Details...</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-medium is-dark">2014</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
+                    </div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">11/2013-09/2014:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">TUM</h1>
+                                    <h2 class="subtitle is-5">Working Student: Energy economy and application technique</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.tum.de/"><img src="/img/logo_tum.png" alt="Logo:TUM-University" width="150" height="79"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_tum','work_details_tum'); this.blur();" id="btn_work_details_tum">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_tum" style="display: none;">
+                                <p> Details... </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
+                    </div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">05/2013-09/2013:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">Fraunhofer IISB</h1>
+                                    <h2 class="subtitle is-5">Working Student: Integrated systems component technology</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.iisb.fraunhofer.de/"><img src="/img/logo_fraunhofer_iisb.png" alt="Logo:Fraunehofer_IISB" width="150" height="43"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_iisb','work_details_iisb'); this.blur();" id="btn_work_details_iisb">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_iisb" style="display: none;">
+                                <p> Details... </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-medium is-dark">2013</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
+                    </div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">10/2012-04/2013:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">Fraunhofer IIS</h1>
+                                    <h2 class="subtitle is-5">Working Student: Geolocation and communication</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.iis.fraunhofer.de/"><img src="/img/logo_fraunhofer_iis.png" alt="Logo:Fraunehofer_IIS" width="150" height="41"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_iis','work_details_iis'); this.blur();" id="btn_work_details_iis">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_iis" style="display: none;">
+                                <p> Details... </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-header">
+                    <span class="tag is-medium is-dark">2012</span>
+                </li>
+                <li class="timeline-item is-info">
+                    <div class="timeline-marker">
+                    </div>
+                    <div class="timeline-content is-100">
+                        <div class="lb is-blue is-step">
+                            <div class="columns">
+                                <div class="column is-2">
+                                    <h1 class="title is-5">10/2011-10/2012:</h1>
+                                </div>
+                                <div class="column is-7 has-text-left">
+                                    <h1 class="title is-3">FAU</h1>
+                                    <h2 class="subtitle is-5">Working Student Tutor: Java Courses</h2>
+                                </div>
+                                <div class="column is-3 has-text-right">
+                                    <a href="https://www.fau.de/"><img src="/img/logo_fau_blue.png" alt="Logo:FAU-University" width="150" height="64"></a>
+                                </div>
+                            </div>
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_fau','work_details_fau'); this.blur();" id="btn_work_details_fau">
+                              <span>Details</span>
+                              <span class="icon is-small">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                              </span>
+                            </button>
+                            <span><br></span>
+                            <div id="work_details_fau" style="display: none;">
+                                <p> Details... </p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                 <li class="timeline-header">
+                    <span class="tag is-large is-info">2011</span>
+                </li>
+            </ul>
 		</div>
-	</div>
-</section>
-<section class="hero">
-	<div class="hero-body">
-		<div class="container">
-			<h1 class="title">Work experience while studying</h1>
-			<div class="lb is-blue is-step">	
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">10/2014-07/2015:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">BMW M GmbH</h1>
-						<h2 class="subtitle is-5">Working Student: OBD department</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.bmw-m.com/de/index.html"><img src="/img/logo_bmw.png" alt="Logo:BMW_M_GmbH" width="100" height="100"></a>
-					</div>
-				</div>
-			</div>
-
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">11/2013-09/2014:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">TUM</h1>
-						<h2 class="subtitle is-5">Working Student: Energy economy and application technique</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.tum.de/"><img src="/img/logo_tum.png" alt="Logo:TUM-University" width="150" height="79"></a>
-					</div>
-				</div>
-			</div>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">05/2013-09/2013:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">Fraunhofer IISB</h1>
-						<h2 class="subtitle is-5">Working Student: Integrated systems component technology</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.iisb.fraunhofer.de/"><img src="/img/logo_fraunhofer_iisb.png" alt="Logo:Fraunehofer_IISB" width="150" height="43"></a>
-					</div>
-				</div>
-			</div>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">10/2012-04/2013:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">Fraunhofer IIS</h1>
-						<h2 class="subtitle is-5">Working Student: Geolocation and communication</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.iis.fraunhofer.de/"><img src="/img/logo_fraunhofer_iis.png" alt="Logo:Fraunehofer_IIS" width="150" height="41"></a>
-					</div>
-				</div>
-			</div>
-			<div class="lb is-blue is-step">
-				<div class="columns">
-					<div class="column is-2">
-						<h1 class="title is-5">10/2011-10/2012:</h1>
-					</div>
-					<div class="column is-7 has-text-left">
-						<h1 class="title is-3">TUM</h1>
-						<h2 class="subtitle is-5">Working Student Tutor: Java Course</h2>
-					</div>
-					<div class="column is-3 has-text-right">
-						<a href="https://www.tum.de/"><img src="/img/logo_tum.png" alt="Logo:TUM-University" width="150" height="79"></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+    </div>
+</section>	
 
 <?php
 	$path = $_SERVER['DOCUMENT_ROOT'];
