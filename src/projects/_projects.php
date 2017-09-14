8a0d192268532644df39fe71bbe70bc9a9822a6c
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-09-14 19:34:12 +0200

    color navbar tab with every color

--- src/projects/_projects.php
+++ src/projects/_projects.php
@@ -27,10 +27,10 @@
 <div class="navbar has-shadow">
 	<div class="container">
 		<div class="navbar-brand">
-			<a href="/projects/website/introduction.php" class="navbar-item is-tab is-red <?php if(isset($projects_website_introduction)){echo $projects_website_introduction;}?>">Introduction</a>
-			<a href="/projects/website/technologies.php" class="navbar-item is-tab is-red <?php if(isset($projects_website_technologies)){echo $projects_website_technologies;}?>">Technologies</a>
-			<a href="/projects/website/environment.php" class="navbar-item is-tab is-red <?php if(isset($projects_website_environment)){echo $projects_website_environment;}?>">Environment</a>
-			<a href="/projects/website/navigation.php" class="navbar-item is-tab is-red <?php if(isset($projects_website_navigation)){echo $projects_website_navigation;}?>">Navigation</a>
+			<a href="/projects/website/introduction.php" class="navbar-item is-tab is-danger <?php if(isset($projects_website_introduction)){echo $projects_website_introduction;}?>">Introduction</a>
+			<a href="/projects/website/technologies.php" class="navbar-item is-tab is-danger <?php if(isset($projects_website_technologies)){echo $projects_website_technologies;}?>">Technologies</a>
+			<a href="/projects/website/environment.php" class="navbar-item is-tab is-danger <?php if(isset($projects_website_environment)){echo $projects_website_environment;}?>">Environment</a>
+			<a href="/projects/website/navigation.php" class="navbar-item is-tab is-danger <?php if(isset($projects_website_navigation)){echo $projects_website_navigation;}?>">Navigation</a>
 		</div>
 	</div>
 </div>
