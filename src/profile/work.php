10fd8cf7969d4381ad6fdb543450c9f53e0642c7
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-09-14 19:29:31 +0200

    lighter nav items, background when active

--- src/profile/work.php
+++ src/profile/work.php
@@ -13,13 +13,13 @@
 			<h1 class="title">Work experience</h1>
             <ul class="timeline">
                 <li class="timeline-header">
-                    <span class="tag is-large is-primary">Present</span>
+                    <span class="tag is-large is-info">Present</span>
                 </li>
-                <li class="timeline-item is-primary">
+                <li class="timeline-item is-info">
                     <div class="timeline-marker">
                     </div>
                     <div class="timeline-content is-100">
-                        <div class="lb is-primary is-active" style="min-width: 200px">
+                        <div class="lb is-info is-active" style="min-width: 200px">
                             <div class="columns">
                                 <div class="column is-2">
                                     <h1 class="title is-5">06/2016-present:</h1>
@@ -33,7 +33,7 @@
                                 </div>
                             </div>
                             
-                            <button class="button is-primary is-outlined" onclick="toggle_details('btn_work_details_bosch','work_details_bosch'); this.blur();" id="btn_work_details_bosch">
+                            <button class="button is-info is-outlined" onclick="toggle_details('btn_work_details_bosch','work_details_bosch'); this.blur();" id="btn_work_details_bosch">
                               <span>Details</span>
                               <span class="icon is-small">
                                 <i class="fa fa-angle-down" aria-hidden="true"></i>
