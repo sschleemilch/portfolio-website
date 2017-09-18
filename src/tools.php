46a80b31cf34bebdba16785af63ff0bbbf12f80b
Sebastian Schleemilch <basti.schleemilch@onlinehome.de>
2017-09-18 20:25:28 +0200

    tools cover

--- src/tools.php
+++ src/tools.php
@@ -5,6 +5,15 @@
 	include_once($path)
 ?>
 
+<section class="hero is-warning is-medium">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <h1 class="title"><span class="icon is-huge"><i class="fa fa-wrench"></i></span></h1>
+            <h1 class="title"><strong>Tools</strong></h1>
+        </div>
+    </div>
+</section>
+
 <section class="section">
 	<div class="container has-text-centered">
 		<span class="icon is-huge"><i class="fa fa-cogs"></i></span>
